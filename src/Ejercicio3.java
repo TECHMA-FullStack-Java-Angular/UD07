90eeed1e194593e8260d0ff986560d682c28c5ff
Paula Sousa <paulasousa137@gmail.com>
2023-03-14 15:12:22 +0100

    Comentarios ejercicio 3

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -13,19 +13,20 @@ import javax.swing.JOptionPane;
 
 public class Ejercicio3 {
 
-	// Array multidimensional para añadir productos y la cantidad de cada;
+	// Lista multidimensional para añadir productos y la cantidad de cada;
 	ArrayList<String[][]> articulos = new ArrayList<String[][]>();
 
+	//Diccionario de datos (articulo: precio)
 	Hashtable<String, Double> catalogoPrecio = new Hashtable<String, Double>();
 
+	//String inicial para introducir articulos a la lista
 	String[][] articulosBase = new String[2][10];
 
-	String[] productosIniciales = { "Avena", "Leche", "Pan", "Arroz", "Chocolate", "Manzana", "Harina", "Pollo",
-			"Shampoo", "Jamón" };
+	
 
 	public void baseDatos() {
 
-		// Añadimos los los articulos iniciales a la base de datos
+		// Añadimos los los articulos iniciales a la Array multidimencional
 
 		articulosBase[0][0] = "avena";
 		articulosBase[1][0] = "100";
@@ -48,6 +49,7 @@ public class Ejercicio3 {
 		articulosBase[0][9] = "jamon";
 		articulosBase[1][9] = "100";
 
+		//Añadimos el array a la lista
 		articulos.add(articulosBase);
 
 		// añadimos los articulos iniciales al catalogo de precios
@@ -62,32 +64,41 @@ public class Ejercicio3 {
 		catalogoPrecio.put("pollo", 6.90);
 		catalogoPrecio.put("shampoo", 4.99);
 		catalogoPrecio.put("jamon", 8.99);
-
+		
+		
+		//Una vez insertados los valores iniciales, activamos la app con un mensaje de bienvenida visual
 		JOptionPane.showMessageDialog(null, "Bienvenido a la APP control de Stock!");
 
+		//Preguntamos si desea introducir nuevo articulo y guardamos el valor en una variable
 		int nuevoArticulo = JOptionPane.showConfirmDialog(null, "¿Deseas introducir nuevo articulo?");
 
+		//En caso positivo, pedimos para la introduccion de nombre, cantidad, precio sin IVA y guardamos 
+		//los valores en variables.
 		if (nuevoArticulo == 0) {
 			String articulo = JOptionPane.showInputDialog("Introduzca el nombre del artículo").toLowerCase();
 			String cantidad = JOptionPane.showInputDialog("Introduzca la cantidad a añadir en stock");
 			String precio = JOptionPane.showInputDialog("Introduzca el precio sin IVA de la unidad.");
 			double precioSt = Double.parseDouble(precio);
 
+			//Añadimos los valores nombre y precio en el diccionario de datos(catalogo precio)
 			catalogoPrecio.put(articulo, precioSt);
 
+			//Añadimos los valores nombre y cantidad en la lista de productos
 			String[][] nuevoPorducto = { { articulo }, { cantidad } };
 			articulos.add(nuevoPorducto);
 
 		}
+		
+		//Preguntamos si desea alterar la cantidad en stock de algun producto
 		int stock = JOptionPane.showConfirmDialog(null, "¿Deseas alterar la cantidad de productos en stock?");
 
+		//En caso positivo solicitamos que ingrese el nombre del producto a alterar y la nueva cantidad en stock
 		if (stock == 0) {
 			String articuloBuscado = JOptionPane
 					.showInputDialog("Introduzca el nombre del artículo que deseas aumentar el stock").toLowerCase();
 			String cantidad = JOptionPane.showInputDialog("Introduzca la cantidad a actualizar en stock");
 
-			// Buscar articulo en articulos y alterar su cantidad
-
+			// Buscar articulo en lista de productos (.equals()) y alterar su cantidad(.valueOf())
 			for (String[][] array : articulos) {
 				for (int i = 0; i < array.length; i++) {
 					for (int j = 0; j < array[i].length; j++) {
@@ -100,14 +111,16 @@ public class Ejercicio3 {
 
 		}
 
-		// Consultar información concreta del diccionario
+		// Preguntamos si desea consultar información concreta del diccionario (catalogoPrecio)
 		int busqueda = JOptionPane.showConfirmDialog(null, "¿Deseas buscar el precio de algun producto?");
 
+		//En caso positivo, solicitamos que introduzca el nombre del articulo a buscar (index)
 		if (busqueda == 0) {
 
 			String articulo = JOptionPane.showInputDialog("Introduzca el nombre del artículo").toLowerCase();
 			Double precio = catalogoPrecio.get(articulo);
 
+			//Si lo encontramos (precio existe) imprimimos el valor
 			if (precio != null) {
 				JOptionPane.showMessageDialog(null, "El precio de " + articulo + " es " + precio);
 
@@ -117,9 +130,10 @@ public class Ejercicio3 {
 			}
 		}
 
-		// Listar informacion Stock
+		// Preguntamos si desea visualizar la lista de informacion Stock
 		int infoStock = JOptionPane.showConfirmDialog(null, "¿Deseas buscar visualizar el stock de productos?");
 
+		//En caso positivo, imprimimos la lista de productos
 		if (infoStock == 0) {
 
 			for (String[][] array : articulos) {
@@ -133,7 +147,7 @@ public class Ejercicio3 {
 			}
 		}
 		
-		//Listar información catálogo productos
+		//Preguntamos si desea visualizar la lista de información precios (diccionario)
 		int infoCatalogo = JOptionPane.showConfirmDialog(null, "¿Deseas buscar visualizar el stock de productos?");
 
 		if (infoCatalogo == 0) {
