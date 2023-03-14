8ace8c3950e0d02888ab2a08b365d6b4e2c3d6eb
Paula Sousa <paulasousa137@gmail.com>
2023-03-14 15:15:00 +0100

    Inicializando ejercicio 4

---
+++ src/Ejercicio4.java
@@ -0,0 +1,8 @@
+/*EJERCICIO 4:
+ * Combina los metodos generados en las actividades 2 y 3 creando una aplicación que gestione 
+ * ventas y control de stock en una misma interfaz. Utiliza para ello las estructuras de datos
+ *  que creas conveniente.*/
+
+public class Ejercicio4 {
+
+}
